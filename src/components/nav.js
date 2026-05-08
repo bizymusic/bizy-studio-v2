7217fb13eae09a9ed7a32a7b1459c35d027a16a3
bizymusic <Zbj458152500@gmail.com>
2026-05-08 10:53:28 +0800

    1

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,23 +9,23 @@ export function renderNav() {
     <nav class="nav" id="nav">
       <div class="nav-inner" id="navInner">
         <div class="nav-links">
-          <a href="index.html">归零</a>
-          <a href="https://music.163.com/#/artist?id=13681128">频率存档</a>
-          <a href="ai-music.html">非人创作</a>
+          <a href="index.html">假装在工作</a>
+          <a href="https://music.163.com/#/artist?id=13681128">正经听歌</a>
+          <a href="ai-music.html">甩锅给AI</a>
 
           <div class="dropdown">
-                <a href="#" class="dropdown-trigger">逻辑构建</a>
+                <a href="#" class="dropdown-trigger">其实我不会</a>
                 <div class="dropdown-menu">
-                    <a href="chordgen.html">和声堆叠</a>
-                    <a href="melodyimprov.html">旋律延伸</a>
+                    <a href="chordgen.html">和弦？不认识</a>
+                    <a href="melodyimprov.html">旋律？随缘吧</a>
                 </div>
           </div>
 
           <div class="dropdown">
-                <a href="#" class="dropdown-trigger">感官映射</a>
+                <a href="#" class="dropdown-trigger">但我会看</a>
                 <div class="dropdown-menu">
-                    <a href="audioview.html">模拟信号</a>
-                    <a href="midiview.html">数字序列</a>
+                    <a href="audioview.html">波形挺好看</a>
+                    <a href="midiview.html">方块挺整齐</a>
                 </div>
           </div>
 
@@ -49,7 +49,7 @@ export function renderNav() {
     const y = e.clientY - (rect.top + rect.height / 2);
     const distance = Math.sqrt(x * x + y * y);
 
-    if (distance < 200) {
+    if (distance < 100) {
       gsap.to(navInner, {
         x: x * 0.1,
         y: y * 0.1,
@@ -64,4 +64,3 @@ export function renderNav() {
   
 
 }
-
